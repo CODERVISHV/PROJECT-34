bb5873f8d039f65f78ca95375c5c6783f64bdad1
CODERVISHV <76100590+CODERVISHV@users.noreply.github.com>
2021-02-28 17:13:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ var database
 var food,foodstock
 
 function preload(){
-  dogimg=loadImage("images/dogImg.png")
-  dogimg2=loadImage("images/dogImg1.png")
+  dogimg=loadImage("dogImg.png")
+  dogimg2=loadImage("dogImg1.png")
 }
 
 function setup(){
